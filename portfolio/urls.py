21ea9d3719f80d0ed3636a43a2d54c14dd17cfea
sjkchang <steven.chang@sjsu.edu>
2020-07-02 20:56:30 -0700

    Made test css file and templates to learn not bootstrap css design

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.home, name='portfolio-home'),
     path('about/', views.about, name='portfolio-about'),
+    path('projects/', views.projects, name='portfolio-projects'),
+    path('resume/', views.resume, name='portfolio-resume'),
+    path('contact/', views.contact, name='portfolio-contact'),
 
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -13,7 +13,28 @@ def home(request):
         'title': 'Home',
         'posts': posts,
     }
-    return render(request, 'portfolio/portfolio-home.html', context)
+    return render(request, 'portfolio/portfolio-temp-base.html', context)
+
+
+def resume(request):
+    context = {
+        'title': 'Resume'
+    }
+    return render(request, 'portfolio/portfolio-resume.html', context)
+
+
+def projects(request):
+    context = {
+        'title': 'Projects'
+    }
+    return render(request, 'portfolio/portfolio-projects.html', context)
+
+
+def contact(request):
+    context = {
+        'title': 'Contact Me'
+    }
+    return render(request, 'portfolio/portfolio-contact.html', context)
 
 
 def about(request):
