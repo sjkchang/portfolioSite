5dedc7748cf5f3456d4c43cf53000919a8d61a70
sjkchang <steven.chang@sjsu.edu>
2020-07-11 20:05:57 -0700

    Mostly finished, ready for deployment

--- portfolio/static/portfolio/js/app.js
+++ portfolio/static/portfolio/js/app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const navLink = document.querySelectorAll('.footer')
 
     burger.addEventListener('click', () => {
         //Toggle Nav
@@ -9,7 +10,7 @@ const navSlide = () => {
 
         //Animate links
         navLinks.forEach((link, index) => {
-            if(link.style.animation){
+            if (link.style.animation) {
                 link.style.animation = ''
             } else {
                 link.style.animation = `navLinkFade 0.25s ease forwards ${index / 7 + .25}s`;
@@ -17,6 +18,17 @@ const navSlide = () => {
         });
     });
 
+    footer.addEventListener('click' , () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.25s ease forwards ${index / 7 + .25}s`;
+            }
+        });
+    });
 
 }
 
